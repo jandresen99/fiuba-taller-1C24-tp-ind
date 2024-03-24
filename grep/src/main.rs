eaf0d7341ea2d33908033ee36a0bc79a447e0e64
Joaquin Andresen <joaquin.andresen@Klar-MacBook-Pro.local>
2024-03-24 17:17:24 -0300

    Agrego funcion para leer archivos

--- grep/src/main.rs
+++ grep/src/main.rs
@@ -1,10 +1,14 @@
 use std::error::Error;
-use grep::utils::read_args;
+use grep::utils::{read_args, read_lines};
 
 fn main() -> Result<(), Box<dyn Error>> {
     let args = read_args()?;
-
     println!("{:?}", args);
+
+    let filepath = &args[2];
+
+    let lines = read_lines(filepath.to_string())?;
+    println!("{:?}", lines);
 
     Ok(())
 }
--- grep/src/utils.rs
+++ grep/src/utils.rs
@@ -1,13 +1,33 @@
 use std::error::Error;
 use std::env;
+use std::fs::File;
+use std::io::BufRead;
+use std::io::BufReader;
+
 
 /// READ_ARGS: lee los argumentos pasados por comando y devuelve una lista con ellos
 pub fn read_args() -> Result<Vec<String>, Box<dyn Error>> {
     let args: Vec<String> = env::args().collect();
 
-    if args.len() <= 1 {
+    if args.len() < 3 {
         Err("Missing arguments")?;
+    } else if  args.len() > 3 {
+        Err("Too many arguments")?;
     }
 
     Ok(args)
+}
+
+/// READ_LINES: lee el archivo indicado y devuelve una lista con cada linea
+pub fn read_lines(filename: String) -> Result<Vec<String>, Box<dyn Error>> {
+    let mut rows = Vec::new();
+    let file = File::open(filename)?;
+
+    let reader = BufReader::new(file);
+
+    for line in reader.lines(){
+        rows.push(line?);
+    }
+
+    Ok(rows)
 }
