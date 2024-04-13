866e15ea9761671a0f8deeb76bd87ac844f6beed
Joaquin Andresen <joaquin.andresen@Klar-MacBook-Pro.local>
2024-04-13 18:04:12 -0300

    Modularizo match del new de Regex

--- grep/src/regex.rs
+++ grep/src/regex.rs
@@ -19,7 +19,6 @@ impl Regex {
 
         for aux_expression in aux_expressions {
             let mut steps: Vec<RegexStep> = vec![];
-            // Recorremos la expression char por char con .next()
             let mut char_iterator = aux_expression.chars();
 
             if !&aux_expression.starts_with('^') {
@@ -35,200 +34,15 @@ impl Regex {
 
             while let Some(c) = char_iterator.next() {
                 let step = match c {
-                    '.' => Some(RegexStep {
-                        rep: RegexRep::Exact(1),
-                        val: RegexVal::Wildcard,
-                    }),
-                    '*' => {
-                        if let Some(last) = steps.last_mut() {
-                            last.rep = RegexRep::Any;
-                        } else {
-                            return Err(Error::new(ErrorKind::Other, "Unexpected '*' found"));
-                        }
-                        None
-                    }
-                    '\\' => match char_iterator.next() {
-                        Some(literal) => Some(RegexStep {
-                            rep: RegexRep::Exact(1),
-                            val: RegexVal::Literal(literal),
-                        }),
-                        None => {
-                            return Err(Error::new(
-                                ErrorKind::Other,
-                                format!("Unexpected character found ({})", c),
-                            ))
-                        }
-                    },
-                    '[' => match char_iterator.next() {
-                        Some(char) => match char {
-                            '[' => {
-                                let next_seven_chars: String =
-                                    char_iterator.by_ref().take(7).collect();
-
-                                let class_type = match next_seven_chars.as_str() {
-                                    ":alnum:" => RegexClass::Alphanumeric,
-                                    ":alpha:" => RegexClass::Alphabetic,
-                                    ":digit:" => RegexClass::Digit,
-                                    ":lower:" => RegexClass::Lowercase,
-                                    ":upper:" => RegexClass::Uppercase,
-                                    ":space:" => RegexClass::Whitespace,
-                                    ":punct:" => RegexClass::Punctuation,
-                                    _ => {
-                                        return Err(Error::new(
-                                            ErrorKind::Other,
-                                            "Unexpected class found",
-                                        ))
-                                    }
-                                };
-
-                                char_iterator.next();
-                                char_iterator.next();
-
-                                Some(RegexStep {
-                                    rep: RegexRep::Exact(1),
-                                    val: RegexVal::Class(class_type),
-                                })
-                            }
-                            _ => {
-                                let mut allowed_chars: Vec<char> = vec![];
-                                let mut not_allowed = false;
-
-                                match char {
-                                    ']' => break,
-                                    '^' => not_allowed = true,
-                                    _ => allowed_chars.push(char),
-                                }
-
-                                while let Some(next_char) = char_iterator.next() {
-                                    match next_char {
-                                        ']' => break,
-                                        _ => allowed_chars.push(next_char),
-                                    }
-                                }
-
-                                if not_allowed {
-                                    Some(RegexStep {
-                                        rep: RegexRep::Exact(1),
-                                        val: RegexVal::NotAllowed(allowed_chars),
-                                    })
-                                } else {
-                                    Some(RegexStep {
-                                        rep: RegexRep::Exact(1),
-                                        val: RegexVal::Allowed(allowed_chars),
-                                    })
-                                }
-                            }
-                        },
-                        None => {
-                            return Err(Error::new(
-                                ErrorKind::Other,
-                                format!("Unexpected character found ({})", c),
-                            ))
-                        }
-                    },
-                    '{' => {
-                        let last_val: RegexVal;
-                        if let Some(last) = steps.last_mut() {
-                            last_val = last.val.clone()
-                        } else {
-                            return Err(Error::new(ErrorKind::Other, "Unexpected '{' found"));
-                        }
-
-                        let mut min = String::new();
-                        let mut max = String::new();
-
-                        let mut min_done = false;
-
-                        while let Some(c) = char_iterator.next() {
-                            if c.is_digit(10) {
-                                if !min_done {
-                                    min.push(c);
-                                } else {
-                                    max.push(c);
-                                }
-                            } else if c == ',' {
-                                min_done = true;
-                                continue;
-                            } else if c == '}' {
-                                break;
-                            } else {
-                                return Err(Error::new(
-                                    ErrorKind::Other,
-                                    format!("Unexpected character found ({}) inside bracket", c),
-                                ));
-                            }
-                        }
-
-                        let min_usize = min.parse::<usize>();
-                        let max_usize = max.parse::<usize>();
-
-                        let final_min: Option<usize>;
-                        let final_max: Option<usize>;
-
-                        match min_usize {
-                            Ok(min_num) => {
-                                final_min = Some(min_num);
-                            }
-                            Err(_err) => {
-                                final_min = None;
-                            }
-                        }
-
-                        match max_usize {
-                            Ok(max_num) => {
-                                final_max = Some(max_num);
-                            }
-                            Err(_err) => {
-                                final_max = None;
-                            }
-                        }
-
-                        Some(RegexStep {
-                            rep: RegexRep::Range(final_min, final_max),
-                            val: last_val,
-                        })
-                    }
-                    '+' => {
-                        let last_val: RegexVal;
-                        if let Some(last) = steps.last_mut() {
-                            last_val = last.val.clone()
-                        } else {
-                            return Err(Error::new(ErrorKind::Other, "Unexpected '+' found"));
-                        }
-
-                        Some(RegexStep {
-                            rep: RegexRep::Any,
-                            val: last_val,
-                        })
-                    }
-                    '?' => {
-                        if let Some(last) = steps.last_mut() {
-                            last.rep = RegexRep::Optional;
-                        } else {
-                            return Err(Error::new(ErrorKind::Other, "Unexpected '?' found"));
-                        }
-                        None
-                    }
-                    '$' => match char_iterator.next() {
-                        None => {
-                            if let Some(last) = steps.last_mut() {
-                                last.rep = RegexRep::Last;
-                            } else {
-                                return Err(Error::new(ErrorKind::Other, "Unexpected '$' found"));
-                            }
-                            None
-                        }
-                        Some(_) => {
-                            return Err(Error::new(ErrorKind::Other, "Unexpected '$' found"))
-                        }
-                    },
-
-                    _ => {
-                        Some(RegexStep {
-                            rep: RegexRep::Exact(1),
-                            val: RegexVal::Literal(c),
-                        })
-                    }
+                    '.' => handle_dot(),
+                    '*' => handle_star(&mut steps)?,
+                    '\\' => handle_backslash(&mut char_iterator)?,
+                    '[' => handle_brace(&mut char_iterator)?,
+                    '{' => handle_bracket(&mut steps, &mut char_iterator)?,
+                    '+' => handle_plus(&mut steps)?,
+                    '?' => handle_question_mark(&mut steps)?,
+                    '$' => handle_end_anchoring(&mut steps, &mut char_iterator)?,
+                    _ => handle_default(c),
                 };
 
                 if let Some(p) = step {
@@ -407,4 +221,222 @@ fn backtrack(
         }
     }
     None
+}
+
+fn handle_dot() -> Option<RegexStep> {
+    return Some(RegexStep {
+        rep: RegexRep::Exact(1),
+        val: RegexVal::Wildcard,
+    });
+}
+
+fn handle_star(steps: &mut Vec<RegexStep>) -> Result<Option<RegexStep>, std::io::Error> {
+    if let Some(last) = steps.last_mut() {
+        last.rep = RegexRep::Any;
+        Ok(None)
+    } else {
+        Err(std::io::Error::new(
+            std::io::ErrorKind::Other,
+            "Unexpected '*' found",
+        ))
+    }
+}
+
+fn handle_backslash(
+    char_iterator: &mut std::str::Chars,
+) -> Result<Option<RegexStep>, std::io::Error> {
+    match char_iterator.next() {
+        Some(literal) => Ok(Some(RegexStep {
+            rep: RegexRep::Exact(1),
+            val: RegexVal::Literal(literal),
+        })),
+        None => {
+            return Err(Error::new(
+                ErrorKind::Other,
+                format!("Unexpected character found \\"),
+            ))
+        }
+    }
+}
+
+fn handle_brace(char_iterator: &mut std::str::Chars) -> Result<Option<RegexStep>, std::io::Error> {
+    match char_iterator.next() {
+        Some(char) => match char {
+            '[' => {
+                let next_seven_chars: String = char_iterator.by_ref().take(7).collect();
+
+                let class_type = match next_seven_chars.as_str() {
+                    ":alnum:" => RegexClass::Alphanumeric,
+                    ":alpha:" => RegexClass::Alphabetic,
+                    ":digit:" => RegexClass::Digit,
+                    ":lower:" => RegexClass::Lowercase,
+                    ":upper:" => RegexClass::Uppercase,
+                    ":space:" => RegexClass::Whitespace,
+                    ":punct:" => RegexClass::Punctuation,
+                    _ => return Err(Error::new(ErrorKind::Other, "Unexpected class found")),
+                };
+
+                char_iterator.next();
+                char_iterator.next();
+
+                Ok(Some(RegexStep {
+                    rep: RegexRep::Exact(1),
+                    val: RegexVal::Class(class_type),
+                }))
+            }
+            _ => {
+                let mut allowed_chars: Vec<char> = vec![];
+                let mut not_allowed = false;
+
+                match char {
+                    ']' => {
+                        return Err(Error::new(
+                            ErrorKind::Other,
+                            format!("Unexpected character found (])"),
+                        ))
+                    }
+                    '^' => not_allowed = true,
+                    _ => allowed_chars.push(char),
+                }
+
+                while let Some(next_char) = char_iterator.next() {
+                    match next_char {
+                        ']' => break,
+                        _ => allowed_chars.push(next_char),
+                    }
+                }
+
+                if not_allowed {
+                    Ok(Some(RegexStep {
+                        rep: RegexRep::Exact(1),
+                        val: RegexVal::NotAllowed(allowed_chars),
+                    }))
+                } else {
+                    Ok(Some(RegexStep {
+                        rep: RegexRep::Exact(1),
+                        val: RegexVal::Allowed(allowed_chars),
+                    }))
+                }
+            }
+        },
+        None => {
+            return Err(Error::new(
+                ErrorKind::Other,
+                format!("No character found after brace"),
+            ))
+        }
+    }
+}
+
+fn handle_bracket(
+    steps: &mut Vec<RegexStep>,
+    char_iterator: &mut std::str::Chars,
+) -> Result<Option<RegexStep>, std::io::Error> {
+    let last_val: RegexVal;
+    if let Some(last) = steps.last_mut() {
+        last_val = last.val.clone()
+    } else {
+        return Err(Error::new(ErrorKind::Other, "Unexpected '{' found"));
+    }
+
+    let mut min = String::new();
+    let mut max = String::new();
+
+    let mut min_done = false;
+
+    while let Some(c) = char_iterator.next() {
+        if c.is_digit(10) {
+            if !min_done {
+                min.push(c);
+            } else {
+                max.push(c);
+            }
+        } else if c == ',' {
+            min_done = true;
+            continue;
+        } else if c == '}' {
+            break;
+        } else {
+            return Err(Error::new(
+                ErrorKind::Other,
+                format!("Unexpected character found ({}) inside bracket", c),
+            ));
+        }
+    }
+
+    let min_usize = min.parse::<usize>();
+    let max_usize = max.parse::<usize>();
+
+    let final_min: Option<usize>;
+    let final_max: Option<usize>;
+
+    match min_usize {
+        Ok(min_num) => {
+            final_min = Some(min_num);
+        }
+        Err(_err) => {
+            final_min = None;
+        }
+    }
+
+    match max_usize {
+        Ok(max_num) => {
+            final_max = Some(max_num);
+        }
+        Err(_err) => {
+            final_max = None;
+        }
+    }
+
+    Ok(Some(RegexStep {
+        rep: RegexRep::Range(final_min, final_max),
+        val: last_val,
+    }))
+}
+
+fn handle_plus(steps: &mut Vec<RegexStep>) -> Result<Option<RegexStep>, std::io::Error> {
+    let last_val: RegexVal;
+    if let Some(last) = steps.last_mut() {
+        last_val = last.val.clone()
+    } else {
+        return Err(Error::new(ErrorKind::Other, "Unexpected '+' found"));
+    }
+
+    Ok(Some(RegexStep {
+        rep: RegexRep::Any,
+        val: last_val,
+    }))
+}
+
+fn handle_question_mark(steps: &mut Vec<RegexStep>) -> Result<Option<RegexStep>, std::io::Error> {
+    if let Some(last) = steps.last_mut() {
+        last.rep = RegexRep::Optional;
+    } else {
+        return Err(Error::new(ErrorKind::Other, "Unexpected '?' found"));
+    }
+    Ok(None)
+}
+
+fn handle_end_anchoring(
+    steps: &mut Vec<RegexStep>,
+    char_iterator: &mut std::str::Chars,
+) -> Result<Option<RegexStep>, std::io::Error> {
+    match char_iterator.next() {
+        None => {
+            if let Some(last) = steps.last_mut() {
+                last.rep = RegexRep::Last;
+            } else {
+                return Err(Error::new(ErrorKind::Other, "Unexpected '$' found"));
+            }
+            Ok(None)
+        }
+        Some(_) => return Err(Error::new(ErrorKind::Other, "Unexpected '$' found")),
+    }
+}
+
+fn handle_default(c: char) -> Option<RegexStep> {
+    return Some(RegexStep {
+        rep: RegexRep::Exact(1),
+        val: RegexVal::Literal(c),
+    });
 }
